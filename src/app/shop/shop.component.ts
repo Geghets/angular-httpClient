817ee5f53cea513899067fe054b8c01c70694eb5
armen96work <armen@investorfuse.com>
2021-05-03 16:35:43 +0400

    data from child to parent with ViewChild

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter, ViewChild, AfterViewInit} from '@angular/core';
 import {OurShopComponent} from './our-shop/our-shop.component';
 
 @Component({
@@ -6,12 +6,18 @@ import {OurShopComponent} from './our-shop/our-shop.component';
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
-export class ShopComponent implements OnInit {
+export class ShopComponent implements OnInit, AfterViewInit {
   public products;
+  public dataFromChild;
 
+  @ViewChild(OurShopComponent) child;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.dataFromChild = this.child.dataTpParent;
   }
 
   receiveData ($event) {
