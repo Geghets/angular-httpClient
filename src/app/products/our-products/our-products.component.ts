f260e97e042c029886ebebe9d928344cc0d5e19a
armen96work <armen@investorfuse.com>
2021-05-05 18:18:28 +0400

    Experimenting with lifecycle hooks

--- src/app/products/our-products/our-products.component.ts
+++ src/app/products/our-products/our-products.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-our-products',
   templateUrl: './our-products.component.html',
   styleUrls: ['./our-products.component.css']
 })
-export class OurProductsComponent implements OnInit {
+export class OurProductsComponent implements OnInit, OnChanges, DoCheck {
   @ Input() productName: string;
 
   messageOutput = 'This is data From Child component in Parent component';
   @Output() messageOutputEvent = new EventEmitter<string>();
 
+  ngOnChanges() {
+    console.log('Change was detected');
+  }
+
   constructor() {
   }
 
   ngOnInit(): void {
+    alert('OnInit is working after OnChanges');
+  }
+
+  ngDoCheck() {
+    console.log('OnCheck is working');
   }
 
   sendMessageOutput () {
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,12 +11,14 @@ export class ProductsComponent implements OnInit {
   public messageFromChildComponent;
   public message: string;
   public modifiedMessage = 'Just Modified data from Product Component';
+  public spreadValues;
 
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService) {}
 
   ngOnInit(): void {
     this.data.currentMessage.subscribe(message => this.message = message);
+    this.spreadValues = this.spreadOperator();
   }
 
   receiveMessage($event) {
@@ -25,6 +27,26 @@ export class ProductsComponent implements OnInit {
 
   productChange() {
     this.data.changeMessage(this.modifiedMessage);
+  }
+
+  spreadOperator () {
+    const array = [1, 5, 7, 2];
+
+    // Edito will not let us use array as parameter without spread operator
+    // const withoutSpread = Math.max(array);
+
+    const withSpread = Math.max(...array);
+
+    const values = {
+      without_spread: 'Will not work or will Return NaN',
+      with_spread: withSpread
+    };
+
+    return values;
+  }
+
+  changeInput() {
+    this.dataFromParentComponent = 'Just changed input data if everything is ok you will se console from ngOnChange';
   }
 
 }
